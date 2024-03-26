f8cfc487d5a8c8d82ae035164251956477a7fc80
eun14 <https://github.com/eun14/haeun.git>
2024-03-26 15:02:05 +0900

    2차 수정

--- ProblemSol/Assets/Scrit/Collection/GameManager.cs
+++ ProblemSol/Assets/Scrit/Collection/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,45 +6,41 @@ public class GameManager : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform greenBox;
     public Transform redBox;
-    public Queue<GameObject> bulletQueue = new Queue<GameObject>();
-
+    public Queue<GameObject> bulletQueue = new Queue<GameObject>();     // 총알을 관리하는 큐
     private bool canShoot = true;
     public int maxBulletCount = 10;
     private int currentBulletCount = 0;
 
     void Start()
     {
-        // 초기에 총알을 생성하여 큐에 추가
-        for (int i = 0; i < maxBulletCount; i++)
+        for (int i = 0; i < maxBulletCount; i++)                        // 초기에 총알을 생성하여 큐에 추가
         {
             GameObject bullet = Instantiate(bulletPrefab, greenBox.position, Quaternion.identity);
-            bullet.SetActive(false);
-            bulletQueue.Enqueue(bullet);
+            bullet.SetActive(false);                                    // 총알을 비활성화하여 숨김
+            bulletQueue.Enqueue(bullet);                                // 총알을 큐에 추가
         }
     }
 
     void Update()
     {
-        // 총알을 발사하는 코드
         if (Input.GetMouseButtonDown(0) && canShoot && currentBulletCount < maxBulletCount)
         {
-            GameObject bullet = bulletQueue.Dequeue();
-            bullet.SetActive(true);
-            bullet.transform.position = greenBox.position;
+            GameObject bullet = bulletQueue.Dequeue();                  // 큐에서 총알을 꺼냄
+            bullet.SetActive(true);                                     // 총알 활성화
+            bullet.transform.position = greenBox.position;              // 발사 위치 지정
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             rb.velocity = (redBox.position - greenBox.position).normalized * 20f;
 
-            ReuseBullet(bullet);
+            ReuseBullet(bullet);                                        // 총알 재활용 매서드 호출
         }
     }
 
-    public void ReuseBullet(GameObject bullet)
+    public void ReuseBullet(GameObject bullet)                          // 총알을 재활용하는 매서드
     {
-        // 재활용된 총알 처리
-        bullet.SetActive(true);
-        bullet.transform.position = greenBox.position;
+        bullet.SetActive(true);                                         // 총알 위치 초기화
+        bullet.transform.position = greenBox.position;                  // 발사 위치 지정
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.velocity = (redBox.position - greenBox.position).normalized * 20f;
-        bulletQueue.Enqueue(bullet); // 재사용된 총알을 다시 큐에 추가
+        bulletQueue.Enqueue(bullet);                                    // 재사용된 총알을 다시 큐에 추가
     }
 }
--- ProblemSol/Assets/Scrit/Collection/Queue.cs
+++ ProblemSol/Assets/Scrit/Collection/Queue.cs
@@ -1,80 +1,42 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 
-public class Node<T>
+namespace DataStrucuture
 {
-    public T data { get; set; }
-    public Node<T> next;
-
-    public Node(T data)
+    public class StackWithQueue<T>
     {
-        this.data = data;
-        this.next = null;
+        private Queue<T> primaryQueue;          // 주 큐
+        private Queue<T> secondaryQueue;        // 보조 큐
+
+        public StackWithQueue()
+        {
+            primaryQueue = new Queue<T>();      // 주 큐 초기화
+            secondaryQueue = new Queue<T>();    // 보조 큐 초기화
+        }
+
+        public void Push(T data)                // 스택에 요소 추가
+        {
+            while (primaryQueue.Count > 0)      // 기존에 있는 요소들을 보조 큐로 옮김
+            {
+                secondaryQueue.Enqueue(primaryQueue.Dequeue());
+            }
+
+            primaryQueue.Enqueue(data);         // 새로운 요소를 주 큐에 추가
+
+            while (secondaryQueue.Count > 0)    // 보조 큐의 모든 요소를 다시 주 큐로 옮김
+            {
+                primaryQueue.Enqueue(secondaryQueue.Dequeue());
+            }
+        }
+
+        public T Pop()                          // 스택에서 요소를 제거하고 반환
+        {
+            if (primaryQueue.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return primaryQueue.Dequeue();      // 주 큐에서 가장 앞에 있는 요소를 꺼내 반환
+        }
     }
 }
-
-public class Queue<T>
-{
-    private Node<T> front;
-    private Node<T> rear;
-
-    public Queue()
-    {
-        front = null;
-        rear = null;
-    }
-
-    public void Enqueue(T data)
-    {
-        Node<T> newNode = new Node<T>(data);
-        if (rear == null)
-        {
-            front = newNode;
-            rear = newNode;
-        }
-        else
-        {
-            rear.next = newNode;
-            rear = newNode;
-        }
-    }
-
-    public T Dequeue()
-    {
-        if (front == null)
-        {
-            Debug.LogError("Queue is empty");
-            return default(T);
-        }
-
-        T data = front.data;
-        front = front.next;
-        if (front == null)
-        {
-            rear = null;
-        }
-        return data;
-    }
-
-    public bool IsEmpty()
-    {
-        return front == null;
-    }
-
-    public T Peek()
-    {
-        if (front == null)
-        {
-            Debug.LogError("Queue is empty");
-            return default(T);
-        }
-        return front.data;
-    }
-}
-
-/*public class LinkedListNode<T>
-{
-    public T Data { get; set; }
-
-    public LinkedListNode<T> 
-}*/
-
